bd34c98774e08285862b0a171980b565058b0d41
Clément Praud <clement.praud6@gmail.com>
2024-11-07 09:32:25 +0100

    Add series route to the rooter, add the series.tsx file, add useParams on series so the displayed content change with the url id

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Series from "./pages/Series";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "/series/:id",
+        element: <Series />,
       },
       {
         path: "/about",
