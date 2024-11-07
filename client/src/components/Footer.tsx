46c736dc335eec7ab972bcdf13ccb95eb3be5766
FAlex357 <alexandrefleurantxbx@gmail.com>
2024-11-07 11:34:33 +0100

    :sparkles: Add footer with logos/CR/CGU

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,25 +7,22 @@ function Footer() {
         <a href="/">&copy;CopyRight</a>
         <section className="logo">
           <a id="logo-insta" href="/">
-            {" "}
             <img
               src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
               alt="logo-instagram"
-            />{" "}
+            />
           </a>
           <a id="logo-fb" href="/">
-            {" "}
             <img
               src="https://cdn-icons-png.flaticon.com/512/59/59439.png"
               alt="logo-facebook"
-            />{" "}
+            />
           </a>
           <a id="logo-gh" href="/">
-            {" "}
             <img
               src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
               alt="logo-github"
-            />{" "}
+            />
           </a>
         </section>
         <a href="/">CGU</a>
