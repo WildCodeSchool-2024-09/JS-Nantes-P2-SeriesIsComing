52f72a89fc20c186f390a2d5e091a77313321e45
Rom3007 <59392770+Rom3007@users.noreply.github.com>
2024-11-08 15:34:41 +0100

    :sparkles: Page ContactUs done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
-
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <ContactUs />
-=======
-      <ContactUs/>
       <h1>Voici App</h1>
       <NavBar />
       <Outlet />
       <Footer />
->>>>>>> dbf8a6f0e8d5b576322c4c9c3086b6bf752acd59
     </>
   );
 }
