4d796dec3f356fd1571575c7fdc9d753ce3f3fb4
FAlex357 <alexandrefleurantxbx@gmail.com>
2024-11-08 09:15:26 +0100

    :sparkles: About page done

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,131 @@
+import "../pages/About.css";
+
 function About() {
-  return <h1>Je suis sur About</h1>;
+  return (
+    <>
+      <p className="a-propos">A propos</p>
+
+      <h1>
+        Les récits nous emportent vers d’autres horizons. Ils éveillent en nous
+        des sentiments inédits, élargissent notre vision du monde et tissent des
+        liens entre nous.
+      </h1>
+
+      <p className="welcome">
+        Bienvenue sur Series is Coming, votre portail incontournable pour
+        plonger dans le monde fascinant des séries ! Découvrez un univers où
+        chaque histoire vous transporte dans des intrigues captivantes et des
+        univers variés, peuplés de personnages attachants et complexes. Ici,
+        chaque série est une aventure, et chaque personnage, un compagnon de
+        voyage.
+      </p>
+
+      <h2>Découvrez l’équipe 🧑🏻‍💻</h2>
+
+      <section className="profil">
+        <section id="emma" className="profil-item">
+          <img
+            className="pic"
+            src="https://www.portraitprofessionnel.fr/wp-content/uploads/2022/02/Portrait_professionnel_CV_et_linkedin_a_Paris.jpg"
+            alt="profile-pic-emma"
+          />
+          <p className="post">Emma Lefevre – Cheffe de projet et UX Designer</p>
+          <p className="role">
+            Rôle : En tant que cheffe de projet, Emma supervise le développement
+            global et garantit la cohérence entre les différentes équipes. Elle
+            est aussi responsable de l'expérience utilisateur (UX) pour
+            s'assurer que la navigation soit fluide et intuitive. Atouts :
+            Excellente communicatrice, vision stratégique, forte capacité
+            d'organisation, et une bonne connaissance des attentes des
+            utilisateurs. Elle sait transformer les idées en expériences
+            interactives et engageantes.
+          </p>
+        </section>
+
+        <section id="lucas" className="profil-item">
+          <img
+            className="pic"
+            src="https://studiolecarre.com/wp-content/uploads/2022/10/2019-03-12-photographe-portrait-corporate-exterieur-2.webp"
+            alt="profile-pic-Lucas"
+          />
+          <p className="post">Lucas Martin – Développeur Full Stack</p>
+          <p className="role">
+            Rôle : En tant que cheffe de projet, Emma supervise le développement
+            global et garantit la cohérence entre les différentes équipes. Elle
+            est aussi responsable de l'expérience utilisateur (UX) pour
+            s'assurer que la navigation soit fluide et intuitive. Atouts :
+            Excellente communicatrice, vision stratégique, forte capacité
+            d'organisation, et une bonne connaissance des attentes des
+            utilisateurs. Elle sait transformer les idées en expériences
+            interactives et engageantes.
+          </p>
+        </section>
+
+        <section id="sofia" className="profil-item">
+          <img
+            className="pic"
+            src="https://studio-mir.fr/wp-content/uploads/2020/04/seance-photo-professionelle-femme-ile-de-france.jpg"
+            alt="profile-pic-sofia"
+          />
+          <p className="post">
+            Sofia Durand – Développeuse Front-End et Web Designer
+          </p>
+          <p className="role">
+            Rôle : En tant que cheffe de projet, Emma supervise le développement
+            global et garantit la cohérence entre les différentes équipes. Elle
+            est aussi responsable de l'expérience utilisateur (UX) pour
+            s'assurer que la navigation soit fluide et intuitive. Atouts :
+            Excellente communicatrice, vision stratégique, forte capacité
+            d'organisation, et une bonne connaissance des attentes des
+            utilisateurs. Elle sait transformer les idées en expériences
+            interactives et engageantes.
+          </p>
+        </section>
+
+        <section id="karim" className="profil-item">
+          <img
+            className="pic"
+            src="https://www.portraitprofessionnel.fr/wp-content/uploads/2022/04/Photographe_trombinoscope_entreprise.jpg"
+            alt="profile-pic-karim"
+          />
+          <p className="post">
+            Karim Ben Ali – Responsable du contenu et spécialiste SEO
+          </p>
+          <p className="role">
+            Rôle : En tant que cheffe de projet, Emma supervise le développement
+            global et garantit la cohérence entre les différentes équipes. Elle
+            est aussi responsable de l'expérience utilisateur (UX) pour
+            s'assurer que la navigation soit fluide et intuitive. Atouts :
+            Excellente communicatrice, vision stratégique, forte capacité
+            d'organisation, et une bonne connaissance des attentes des
+            utilisateurs. Elle sait transformer les idées en expériences
+            interactives et engageantes.
+          </p>
+        </section>
+
+        <section id="julie" className="profil-item">
+          <img
+            className="pic"
+            src="https://static.wixstatic.com/media/331f88_3929f9f015014ba58e48a9aba94e3fe4~mv2.jpg/v1/fill/w_980,h_972,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/331f88_3929f9f015014ba58e48a9aba94e3fe4~mv2.jpg"
+            alt="profile-pic-julie"
+          />
+          <p className="post">
+            Julie Nguyen – Développeuse Back-End et Responsable Data
+          </p>
+          <p className="role last-role">
+            Rôle : En tant que cheffe de projet, Emma supervise le développement
+            global et garantit la cohérence entre les différentes équipes. Elle
+            est aussi responsable de l'expérience utilisateur (UX) pour
+            s'assurer que la navigation soit fluide et intuitive. Atouts :
+            Excellente communicatrice, vision stratégique, forte capacité
+            d'organisation, et une bonne connaissance des attentes des
+            utilisateurs. Elle sait transformer les idées en expériences
+            interactives et engageantes.
+          </p>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default About;
