d30d446fd65938ae209c1a3075d0753b36d1d6e2
Clément Praud <clement.praud6@gmail.com>
2024-11-12 12:35:33 +0100

    Fix css style files

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-      <Link to={"/"}>
+      <Link to={"/"} className="link-nav">
         <img
           src="https://th.bing.com/th/id/R.ca8a3618d313baa5d52322bc4b437dad?rik=4Xq0pWjDTNTuDA&pid=ImgRaw&r=0"
           alt="logo"
@@ -14,10 +14,14 @@ function NavBar() {
 
       <ul>
         <li>
-          <Link to={"/about"}>A propos</Link>
+          <Link to={"/about"} className="link-nav">
+            A propos
+          </Link>
         </li>
         <li>
-          <Link to={"/contact"}>Nous contacter</Link>
+          <Link to={"/contact"} className="link-nav">
+            Nous contacter
+          </Link>
         </li>
         <li className="dropdown">
           <h3>
@@ -26,16 +30,24 @@ function NavBar() {
 
           <ul className="dropdown-content">
             <li>
-              <Link to={"/series/1"}>Game of Thrones</Link>
+              <Link to={"/series/1"} className="link-nav">
+                Game of Thrones
+              </Link>
             </li>
             <li>
-              <Link to={"/series/2"}>Série 2</Link>
+              <Link to={"/series/2"} className="link-nav">
+                Série 2
+              </Link>
             </li>
             <li>
-              <Link to={"/series/3"}>Série 3</Link>
+              <Link to={"/series/3"} className="link-nav">
+                Série 3
+              </Link>
             </li>
             <li>
-              <Link to={"/series/4"}>Série 4</Link>
+              <Link to={"/series/4"} className="link-nav">
+                Série 4
+              </Link>
             </li>
           </ul>
         </li>
