017cc6d9a83a953bffa70efae712e08a0067e8c7
Clément Praud <clement.praud6@gmail.com>
2024-11-12 14:17:44 +0100

    change link to navlink

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,27 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
-      <Link to={"/"} className="link-nav">
+      <NavLink to={"/"} className="link-nav">
         <img
           src="https://th.bing.com/th/id/R.ca8a3618d313baa5d52322bc4b437dad?rik=4Xq0pWjDTNTuDA&pid=ImgRaw&r=0"
           alt="logo"
           id="logo"
         />
-      </Link>
+      </NavLink>
       <h1>Serie is Comming</h1>
 
       <ul>
         <li>
-          <Link to={"/about"} className="link-nav">
+          <NavLink to={"/about"} className="link-nav">
             A propos
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to={"/contact"} className="link-nav">
+          <NavLink to={"/contact"} className="link-nav">
             Nous contacter
-          </Link>
+          </NavLink>
         </li>
         <li className="dropdown">
           <h3>
@@ -30,24 +30,24 @@ function NavBar() {
 
           <ul className="dropdown-content">
             <li>
-              <Link to={"/series/1"} className="link-nav">
+              <NavLink to={"/series/1"} className="link-nav">
                 Game of Thrones
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to={"/series/2"} className="link-nav">
+              <NavLink to={"/series/2"} className="link-nav">
                 Série 2
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to={"/series/3"} className="link-nav">
+              <NavLink to={"/series/3"} className="link-nav">
                 Série 3
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to={"/series/4"} className="link-nav">
+              <NavLink to={"/series/4"} className="link-nav">
                 Série 4
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </li>
