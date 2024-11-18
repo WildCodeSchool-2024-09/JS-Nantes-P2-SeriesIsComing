b43d981578ea3edcebecb55027965de84f600ef2
Rom3007 <59392770+Rom3007@users.noreply.github.com>
2024-11-18 16:15:39 +0100

    card animation done

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,25 +1,47 @@
-// Import style sheet
+import { useState } from "react";
 import "./Card.css";
-
-// Import data interfaces
 import type dataI from "../assets/interfaces/dataI";
-function Card({ character, id }: { character: dataI[]; id: string }) {
+
+interface CardProps {
+  character: dataI[];
+}
+
+const Card: React.FC<CardProps> = ({ character }) => {
+  const [flippedStates, setFlippedStates] = useState<Record<number, boolean>>(
+    {},
+  );
+
+  const handleFlip = (charId: number): void => {
+    setFlippedStates((prevStates) => ({
+      ...prevStates,
+      [charId]: !prevStates[charId],
+    }));
+  };
+
   return (
     <section>
       <div className="card-container">
         {character.map((charac) => (
-          <article className={`card-${id}`} key={charac.id}>
-            <figure>
-              <img src={charac.imageUrl} alt="This is a representation" />
-              <figcaption>
-                {charac.firstName} {charac.lastName}
-              </figcaption>
-            </figure>
-          </article>
+          <button
+            type="button"
+            onClick={() => handleFlip(charac.id)}
+            key={charac.id}
+            className={`card-inner ${flippedStates[charac.id] ? "flipped" : ""}`}
+          >
+            <div className="card-front">
+              <img
+                src={charac.imageUrl}
+                alt={`${charac.firstName} ${charac.lastName}`}
+              />
+            </div>
+            <div className="card-back">
+              <p>{charac.description || "Information indisponible"}</p>
+            </div>
+          </button>
         ))}
       </div>
     </section>
   );
-}
+};
 
 export default Card;
