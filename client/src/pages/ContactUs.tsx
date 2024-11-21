7d4119d970441abf991d4467848726d7891cdf33
Clément Praud <clement.praud6@gmail.com>
2024-11-21 16:57:19 +0100

    Add onSubmit event on ContactUs.tsx to retrieve user's field

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -1,26 +1,54 @@
 import "../pages/ContactUs.css";
+import type { FormEvent } from "react";
+
 function ContactUs() {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (event.target) {
+      const formData = new FormData(event.currentTarget);
+      const formattedData = Object.fromEntries(formData);
+      console.warn(formattedData);
+    }
+  };
+
   return (
     <div className="div-contact-us">
       <section className="section-1">
-        <div className="left-part">
-          <input className="name_contact_user" type="text" placeholder="Nom" />
+        <form onSubmit={onSubmit} className="left-part">
           <input
+            className="name_contact_user"
+            type="text"
+            placeholder="Nom"
+            name="userFirstName"
+          />
+          <input
+            name="userLastName"
             className="first_name-contact_user"
             type="text"
             placeholder="Prenom"
           />
           <input
+            name="userMail"
             className="mail_contact_user"
             type="text"
             placeholder="Adresse mail"
           />
-        </div>
+          <label htmlFor="seriesName">
+            Des series que tu souhaite voire sur le site ?
+          </label>
+          <input
+            type="text"
+            placeholder="Nom d'une série"
+            name="seriesName"
+            id="seriesName"
+          />
+
+          <button type="submit">Valider</button>
+        </form>
+
         <div className="right-part">
-          <p>
-            <strong>Des series que tu souhaite voire sur le site ?</strong>
-          </p>
-          <input type="text" placeholder="Nom d'une série" />
+          <p>Des series que tu souhaite voire sur le site ?</p>
+
           <input type="text" />
         </div>
       </section>
@@ -33,18 +61,12 @@ function ContactUs() {
           />
         </div>
         <div className="buttons-contain">
-          <button className="contact-button" type="button">
-            Abbone toi à notre newlester
-          </button>
-          <button className="contact-button" type="button">
-            Valide ta demande
-          </button>
+          <button type="button">Abbone toi à notre newlester</button>
+          <button type="button">Valide ta demande</button>
         </div>
       </section>
       <div className="mentions-contain">
-        <button className="contact-button" type="button">
-          Mentions légales
-        </button>
+        <button type="button">Mentions légales</button>
       </div>
     </div>
   );
