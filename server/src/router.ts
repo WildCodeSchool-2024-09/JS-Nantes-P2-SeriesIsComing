fb8f54a9c599dceaf5eaaa7b0c7d5be081687dc4
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-11-22 15:31:36 +0100

    🎉 API done - The team will be able to fetch data from endpoint /api/series

--- server/src/router.ts
+++ server/src/router.ts
@@ -6,6 +6,14 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
+// API route for Series is Coming project
+
+import seriesData from "./assets/seriesData";
+
+router.get("/api/series", (_, res) => {
+  res.status(200).send(seriesData);
+});
+
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
 
