ec63d1e3d510592507e33eb49c5d0faecf3e0400
quentinlqs <quentinloquais44@gmail.com>
2024-11-26 15:12:05 +0100

    change the logo in relation to the page where you are

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,28 +1,57 @@
 import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useUser from "../utils/useUser";
 
 function NavBar() {
   const { user } = useUser();
-
+  const { id } = useParams<string>();
+  const logos = [
+    {
+      id: 1,
+      image:
+        "https://www.pngplay.com/wp-content/uploads/13/Game-Of-Thrones-PNG-Pic-Background.png",
+    },
+    {
+      id: 2,
+      image:
+        "https://logowik.com/content/uploads/images/the-walking-dead3382.jpg",
+    },
+    {
+      id: 3,
+      image:
+        "https://seeklogo.com/images/B/breaking-bad-logo-7466E7CD42-seeklogo.com.png",
+    },
+    {
+      id: 4,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/9/9c/Prisonbreak.png",
+    },
+  ];
+  const logoSeries = logos.find((logo) => logo.id === Number(id));
+  const defaultLogo =
+    "https://www.pngplay.com/wp-content/uploads/7/Home-Logo-Background-PNG-Image.png";
   return (
     <nav>
-      <NavLink to={"/"} className="NavLink-nav">
-        <img
-          src="https://th.bing.com/th/id/R.ca8a3618d313baa5d52322bc4b437dad?rik=4Xq0pWjDTNTuDA&pid=ImgRaw&r=0"
-          alt="logo"
-          id="logo"
-        />
-      </NavLink>
-      <h1 id="title">
+      <div className="logofit">
+        <NavLink to={"/"} className="NavLink-nav">
+          <img
+            src={logoSeries?.image ? logoSeries?.image : defaultLogo}
+            alt="logo"
+            className="logo"
+          />
+        </NavLink>
+      </div>
+
+      <h1 className="title">
         {user ? `Hello ${user.userFirstName}` : "Series is Coming"}
       </h1>
       <ul>
         <li>
           <NavLink to={"/about"} className="NavLink-nav">
             {" "}
-            <h3 className="h3-text">A propos</h3>
+            <h3 className="h3-text">À propos</h3>
             <img
-              id="logo2"
+              className="logo2"
               src="https://cdn-icons-png.flaticon.com/512/6229/6229050.png"
               alt="point d'interrogation"
             />
@@ -33,7 +62,7 @@ function NavBar() {
             {" "}
             <h3 className="h3-text">Nous contacter</h3>
             <img
-              id="logo2"
+              className="logo2"
               src="https://th.bing.com/th/id/R.74fef23e2e02b874f883b8fbd3847cc8?rik=3Vaj6GaDSb4VoQ&riu=http%3a%2f%2ficon-library.com%2fimages%2ftelephone-icon-png%2ftelephone-icon-png-1.jpg&ehk=Af6dFLM25381YRPy0lGgLzwUdSpzy%2bdyrILCDHig6fg%3d&risl=&pid=ImgRaw&r=0"
               alt="telephone portable"
             />
@@ -42,10 +71,10 @@ function NavBar() {
 
         <li className="dropdown">
           {" "}
-          <h3 className="h3-text"> Plus de Série</h3>
+          <h3 className="h3-text"> Plus de Séries</h3>
           <div className="arrow">▼</div>
           <img
-            id="logo2"
+            className="logo2"
             src="https://cdn-icons-png.flaticon.com/512/275/275201.png"
             alt="telephone portable"
           />
