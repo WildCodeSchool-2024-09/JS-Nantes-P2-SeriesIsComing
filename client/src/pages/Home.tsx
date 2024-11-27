27771b6affbc67e3ed4b30567174f009fb0743c4
Clément Praud <clement.praud6@gmail.com>
2024-11-27 15:00:23 +0100

    Review of Home.tsx : export the data + replace variable names by more semantique ones

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,50 +1,24 @@
-import "./Home.css";
+// Import React Modules
 import { Link } from "react-router-dom";
 
-interface Article {
-  image: string;
-  altText: string;
-  link: string;
-}
+// Import Data
+import { mainSeriesCards } from "../assets/data/mainSeriesCards";
 
-const Home: React.FC = () => {
-  const articles: Article[] = [
-    {
-      image:
-        "https://th.bing.com/th/id/R.0130ebbdd9706106e8c6904611e87765?rik=%2bU1w5z%2f10v6CkQ&pid=ImgRaw&r=0",
-      altText: "First series : Game of Thrones",
-      link: "/series/1",
-    },
-    {
-      image:
-        "https://th.bing.com/th/id/OIP.DvMkNmw3ZHi8X2E9Fqi4wgHaHa?rs=1&pid=ImgDetMain",
-      altText: "Second series : The Walking Dead",
-      link: "/series/2",
-    },
-    {
-      image:
-        "https://lasa9ni.shop/cdn/shop/files/7A0177BB-2FB1-4F00-BC18-19BDE0DC2350.jpg?v=1708208390",
-      altText: "Third series : Breaking Bad",
-      link: "/series/3",
-    },
-    {
-      image: "https://karoo.me/site/assets/files/19358/prison-break.jpg",
-      altText: "Fourth series : Prison Break",
-      link: "/series/4",
-    },
-  ];
+// Import Style Sheet
+import "./Home.css";
 
+function Home() {
   return (
     <main>
-      <section id="recent-articles">
-        {articles.map((article) => (
-          <Link to={article.link} key={article.link} className="clickable-card">
-            <img src={article.image} alt={article.altText} />
+      <section id="main-series-cards">
+        {mainSeriesCards.map((card) => (
+          <Link to={card.link} key={card.link} className="clickable-card">
+            <img src={card.image} alt={card.altText} />
           </Link>
         ))}
       </section>
     </main>
   );
-};
+}
 
 export default Home;
