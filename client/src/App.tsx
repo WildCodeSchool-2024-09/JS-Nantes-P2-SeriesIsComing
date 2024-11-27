40fe1b83527dc08b2f016be36f71d7954434a65d
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-11-27 11:08:44 +0100

    editing favorite page and navbar and sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import "./components/Navbar.css";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-
+import "./assets/style/animation.css";
 function App() {
   return (
     <>
--- client/src/pages/Favorite.tsx
+++ client/src/pages/Favorite.tsx
@@ -2,21 +2,21 @@ import "./Favorite.css";
 
 function Favorite() {
   return (
-    <div className="app">
-      <div className="card-container">
-        <div className="card">
+    <div className="favorite">
+      <section className="card-container">
+        <article className="card">
           <h2>favorit1</h2>
-        </div>
-        <div className="card">
+        </article>
+        <article className="card">
           <h2>favorit2</h2>
-        </div>
-        <div className="card">
+        </article>
+        <article className="card">
           <h2>favorit3</h2>
-        </div>
-        <div className="card">
+        </article>
+        <article className="card">
           <h2>favorit4</h2>
-        </div>
-      </div>
+        </article>
+      </section>
     </div>
   );
 }
