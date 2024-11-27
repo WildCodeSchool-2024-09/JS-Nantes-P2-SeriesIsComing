a9bfe6e3bfda66070ac55d7080999a07e3e32451
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-11-27 14:02:31 +0100

    editing the footer page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,28 +4,39 @@ function Footer() {
   return (
     <>
       <footer>
-        <a href="/">&copy;CopyRight</a>
+        <a id="logo1" href="/">
+          &copy;CopyRight
+        </a>
         <section className="logo">
-          <a id="logo-insta" href="/">
+          <a id="logo-insta" href="https://www.instagram.com/">
             <img
-              src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
+              src="https://www.clipartmax.com/png/middle/4-41427_instagram-png-icon-instagram-logo-transparent.png"
               alt="logo-instagram"
             />
           </a>
-          <a id="logo-fb" href="/">
+          <a id="logo-fb" href="https://www.facebook.com/">
             <img
-              src="https://cdn-icons-png.flaticon.com/512/59/59439.png"
+              src="https://e7.pngegg.com/pngimages/339/980/png-clipart-social-media-computer-icons-facebook-graphics-social-media-blue-text.png"
               alt="logo-facebook"
             />
           </a>
-          <a id="logo-gh" href="/">
+          <a id="logo-X" href="https://x.com/Accueil">
+            <img
+              src="https://i.pinimg.com/736x/69/a6/2a/69a62a5edc08d755dd8a4ef017e14c63.jpg"
+              alt="logo-twitter"
+            />
+          </a>
+
+          <a id="logo-gh" href="https://github.com/">
             <img
               src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
               alt="logo-github"
             />
           </a>
         </section>
-        <a href="/">CGU</a>
+        <a id="logo1" href="/">
+          CGU
+        </a>
       </footer>
     </>
   );
