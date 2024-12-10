d28077be14d6e1cd0576b60894706b20abb18880
Clément Praud <clement.praud6@gmail.com>
2024-12-10 14:19:24 +0100

    adjust scroll-to-top-button position and color

--- client/src/components/ScrollToTopButton.tsx
+++ client/src/components/ScrollToTopButton.tsx
@@ -48,7 +48,7 @@ const ScrollToTopButton = () => {
       <div
         className="progress-circle"
         style={{
-          borderImage: `conic-gradient(#000000 ${scrollProgress}%, transparent 0%) 1`,
+          borderImage: `conic-gradient(white ${scrollProgress}%, transparent 0%) 1`,
         }}
       >
         <button onClick={scrollToTop} type="button">
